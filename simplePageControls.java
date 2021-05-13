708ad6aa27093727ed8091265ac00be22d40f21c
ThomasTestingTechnology <69004131+ThomasTestingTechnology@users.noreply.github.com>
2021-05-13 16:09:13 +0100

    Add files via upload

---
+++ simplePageControls.java
@@ -0,0 +1,18 @@
+package simpleWebsite;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+/* in this class we extend the abstract class of test methods for common controls, which can be extended
+ * with methods peculiar to the specific project. This is also instantiated in the page methods class
+ */
+
+public class simplePageControls extends abstractControlTestMethods {
+	
+}
